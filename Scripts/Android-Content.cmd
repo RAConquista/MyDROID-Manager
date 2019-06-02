c65e65b9a1af1f7aba17a35e791c8ae299e615d3
RAConquista <44280330+RAConquista@users.noreply.github.com>
2019-06-02 20:56:44 +0200

    Update Android-Content.cmd

--- Scripts/Android-Content.cmd
+++ Scripts/Android-Content.cmd
@@ -12,7 +12,8 @@ echo.
 echo [1] Game of Thrones: Conquest. 
 echo [2] SparkChess Pro 
 echo [3] City Island 3 (Money Mod) 
-echo [4] RISIKO: Global Domination 
+echo [4] RISIKO: Global Domination
+echo [5] GTA: Liberty City Stories
 echo.
 
 set asw=0
@@ -22,6 +23,7 @@ if %asw%==1 goto App1
 if %asw%==2 goto App2
 if %asw%==3 goto App3
 if %asw%==4 goto App4
+if %asw%==5 goto App5
 
 goto END
 
@@ -59,14 +61,28 @@ powershell -Command "(New-Object Net.WebClient).DownloadFile('http://s3.rexdl.co
 powershell -Command "Invoke-WebRequest http://s3.rexdl.com/android/game/City-Island-3-Building-Sim-v3.0.5-Mod-www.ReXdl.com.apk -OutFile Downloads\City-Island-3-Building-Sim-v3.0.5-Mod.apk" 2>nul;
 echo.
 
+goto Downloadmenu
+
 :App4
 cls
 echo.
-echo City Island 3 wird geladen... (Kann einige Zeit in anspruch nehmen)
+echo RISIKO: Global Domination wird geladen... (Kann einige Zeit in anspruch nehmen)
 if exist Downloads\RISK-Global-Domination-v1.24.66.477.apk 
 del Downloads\RISK-Global-Domination-v1.24.66.477.apk
 powershell -Command "(New-Object Net.WebClient).DownloadFile('http://s1.rexdl.com/android/game/RISK-Global-Domination-v1.24.66.477-Mod-www.ReXdl.com.apk', 'Downloads\RISK-Global-Domination-v1.24.66.477.apk')" 2>nul
 powershell -Command "Invoke-WebRequest http://s1.rexdl.com/android/game/RISK-Global-Domination-v1.24.66.477-Mod-www.ReXdl.com.apk -OutFile Downloads\RISK-Global-Domination-v1.24.66.477.apk" 2>nul;
+echo.
+
+goto Downloadmenu
+
+:App5
+cls
+echo.
+echo GTA: Liberty City Stories v2.4 wird geladen... (Kann einige Zeit in anspruch nehmen)
+if exist Downloads\GTA-Liberty-City-Stories-v2.4-Mod.apk
+del Downloads\GTA-Liberty-City-Stories-v2.4-Mod.apk
+powershell -Command "(New-Object Net.WebClient).DownloadFile('http://s5.rexdl.com/android/game/GTA-Liberty-City-Stories-v2.4-Mod-www.ReXdl.com.apk', 'Downloads\GTA-Liberty-City-Stories-v2.4-Mod.apk')" 2>nul
+powershell -Command "Invoke-WebRequest http://s5.rexdl.com/android/game/GTA-Liberty-City-Stories-v2.4-Mod-www.ReXdl.com.apk -OutFile Downloads\GTA-Liberty-City-Stories-v2.4-Mod.apk" 2>nul;
 echo.
 
 goto Downloadmenu
